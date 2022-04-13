f58a1745d87ec3ca95c4e956a763290d4741eb81
William Meyer <valodier@icloud.com>
2022-04-13 09:49:46 -0400

    Adds index and create paths to script/template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Look at all the Places!</h1>
+    <div>
+      <h4>Create a place.</h4>
+      Name:
+      <input type="text" v-model="newPlacesParams.name" />
+      Address:
+      <input type="text" v-model="newPlacesParams.address" />
+      <br />
+      <button v-on:click="placesCreate()">BRING FORTH</button>
+    </div>
+    <div v-for="place in places" :key="place.id">
+      <h2>{{ place.name }}</h2>
+      <h3>{{ place.address }}</h3>
+      <br />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [],
+      newPlacesParams: {},
+      errors: [],
+    };
+  },
+  created: function () {
+    axios.get("/places").then((response) => {
+      this.places = response.data;
+      console.log("Places Index", this.places);
+    });
+  },
+  methods: {
+    placesCreate: function () {
+      axios
+        .post("/places", this.newPlacesParams)
+        .then((response) => {
+          console.log("Good news!", response.data);
+          this.places.push(response.data);
+          this.newPlacesParams = {};
+        })
+        .catch((error) => {
+          this.error = error.response.data.errors;
+        });
+    },
   },
 };
 </script>
+
+<style></style>
